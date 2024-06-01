45eab261aaff6d26b2266ba4e9926dbcddc7ad53
Ganesh Dole <dole77770@gmail.com>
2024-06-01 20:06:13 +0530

    fix scroll issue

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -2,9 +2,12 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import ScrollTop from "@ganeshdole/scrolltotop";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
-    <App />
+    <ScrollTop>
+      <App />
+    </ScrollTop>
   </BrowserRouter>
 );
